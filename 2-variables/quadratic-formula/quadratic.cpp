97ff74db00e73a22c7a7d5e26176f8bb78f38211
Sonny Li <sonnynomnom@gmail.com>
2019-03-25 15:39:34 -0400

    `sqrt()` -> `std::sqrt`
    `std::sqrt()` should be used instead of just `sqrt()` because `<cmath>` doesn't necessarily provide `sqrt()` in the global namespace.

--- 2-variables/quadratic-formula/quadratic.cpp
+++ 2-variables/quadratic-formula/quadratic.cpp
@@ -15,8 +15,8 @@ int main() {
   std::cout << "Enter c: ";
   std::cin >> c;
 
-  root1 = (-b + sqrt(b*b - 4*a*c)) / (2*a);
-  root2 = (-b - sqrt(b*b - 4*a*c)) / (2*a);
+  root1 = (-b + std::sqrt(b*b - 4*a*c)) / (2*a);
+  root2 = (-b - std::sqrt(b*b - 4*a*c)) / (2*a);
 
   std::cout << "Root 1 is " << root1 << "\n";
   std::cout << "Root 2 is " << root2 << "\n";
