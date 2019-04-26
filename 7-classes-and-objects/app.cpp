4264c45b2cdceb6072211a4a5b4f11223dc835ba
marielfrank <marielbeth@gmail.com>
2019-04-26 13:16:04 -0400

    Add classes and objects

---
+++ 7-classes-and-objects/app.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "profile.hpp"
+
+int main() {
+
+  Profile sam("Sam Drakkila", 30, "New York", "USA", "he/him");
+  sam.add_hobby("listening to audiobooks and podcasts");
+  sam.add_hobby("playing rec sports like bowling and kickball");
+  sam.add_hobby("writing a speculative fiction novel");
+  sam.add_hobby("reading advice columns");
+  std::cout << sam.view_profile();
+
+}
