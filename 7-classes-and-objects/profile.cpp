de1331fc07070e071bd189cece46bc7f889e287b
marielfrank <marielbeth@gmail.com>
2019-05-08 18:30:08 -0400

    Fix constructor solution

--- 7-classes-and-objects/profile.cpp
+++ 7-classes-and-objects/profile.cpp
@@ -2,13 +2,14 @@
 
 #include "profile.hpp"
 
-Profile::Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns) {
+Profile::Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns)
+  : name(new_name), age(new_age), city(new_city), country(new_country), pronouns(new_pronouns) {
 
-  name = new_name;
-  age = new_age;
-  city = new_city;
-  country = new_country;
-  pronouns = new_pronouns;
+  if (new_age >= 18) {
+    age = new_age;
+  } else {
+    age = 0;
+  }
 
 }
 
