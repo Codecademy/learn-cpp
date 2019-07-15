7b62472341b712e04b248d7d2b175816fade3e91
Ali Parandeh <43654026+Ali-Parandeh@users.noreply.github.com>
2019-07-15 01:30:55 +0100

    Add full UFO Project
    Include all functionalities

---
+++ 6-functions/UFO/ufo_functions.hpp
@@ -0,0 +1,8 @@
+#include <vector>
+#include <string>
+
+// Declaring functions
+void display_misses(int misses);
+void endgame(std::string answer, std::string codeword);
+void display_status(std::vector<char> incorrect, std::string answer);
+void greet();
