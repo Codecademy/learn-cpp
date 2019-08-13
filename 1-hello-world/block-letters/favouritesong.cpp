8dfd0543b71aa6c25ed68e8a676f95e8cd46c6a2
Sophia <TheGiraffe@users.noreply.github.com>
2019-08-13 20:15:19 +0100

    Added an extra example to the block letters folder.

---
+++ 1-hello-world/block-letters/favouritesong.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+//Fun fact, I am not very interesting
+int main(){
+  std::cout << "DDDD   IIIII   GGG   IIIII  TTTTT  A    L     \n";
+  std::cout << "D   D    I    G   G    I      T   A A   L     \n";
+  std::cout << "D   D    I    G        I      T  A   A  L     \n";
+  std::cout << "D   D    I    G GGG    I      T  AAAAA  L     \n";
+  std::cout << "D   D    I    G   G    I      T  A   A  L     \n";
+  std::cout << "D   D    I    G   G    I      T  A   A  L     \n";
+  std::cout << "DDDD   IIIII   GGG   IIIII    T  A   A  LLLLL \n";
+  std::cout << "         L       OOO   V   V  EEEEE           \n";
+  std::cout << "         L      O   O  V   V  E               \n";
+  std::cout << "         L      O   O  V   V  E               \n";
+  std::cout << "         L      O   O  V   V  EEE             \n";
+  std::cout << "         L      O   O  V   V  E               \n";
+  std::cout << "         L      O   O   V V   E               \n";
+  std::cout << "         LLLLL   OOO     V    EEEEE           \n";
+  std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+  std::cout << "             #######    #######               \n";
+  std::cout << "           ##########  ##########             \n";
+  std::cout << "           ######################             \n";
+  std::cout << "            ####################              \n";
+  std::cout << "             ##################               \n";
+  std::cout << "                ############                  \n";
+  std::cout << "                  ########                    \n";
+  std::cout << "                     ##                       \n";
+  std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+  std::cout << "DDDD      A    FFFFF  TTTTT                   \n";
+  std::cout << "D   D    A A   F        T                     \n";
+  std::cout << "D   D   A   A  F        T                     \n";
+  std::cout << "D   D   AAAAA  FFF      T                     \n";
+  std::cout << "D   D   A   A  F        T                     \n";
+  std::cout << "D   D   A   A  F        T                     \n";
+  std::cout << "DDDD    A   A  F        T                     \n";
+  std::cout << "               PPPP   U   U  N   N  K   K     \n";
+  std::cout << "               P   P  U   U  NN  N  K  K      \n";
+  std::cout << "               P   P  U   U  N N N  K K       \n";
+  std::cout << "               PPPP   U   U  N  NN  KK        \n";
+  std::cout << "               P      U   U  N   N  K K       \n";
+  std::cout << "               P      U   U  N   N  K  K      \n";
+  std::cout << "               P       UUU   N   N  K   K     \n";
+}
