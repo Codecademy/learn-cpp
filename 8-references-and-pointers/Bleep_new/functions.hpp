1463d8d5f77a4e7f998e1e321c693a307a118ca8
AnOnYmOus001100 <47621801+AnOnYmOus001100@users.noreply.github.com>
2020-09-22 13:13:05 +0530

    Update functions.hpp

--- 8-references-and-pointers/Bleep_new/functions.hpp
+++ 8-references-and-pointers/Bleep_new/functions.hpp
@@ -1,2 +1,4 @@
+using namespace std;
+
 // declaring bleep function, with return type and two string parameters word (passed as value) and text (passed as reference) 
-std::string bleep(std::string word, std::string &text);
+string bleep(string word, string &text);
