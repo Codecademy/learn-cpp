9fc5d7d7d81378d14f3756057aee82e10a4bacce
anapaulaonc <54750948+anapaulaonc@users.noreply.github.com>
2021-04-02 15:23:08 -0300

    Update quadratic.cpp

--- 2-variables/quadratic-formula/quadratic.cpp
+++ 2-variables/quadratic-formula/quadratic.cpp
@@ -2,25 +2,20 @@
 #include <cmath>
 
 int main() {
-
+  
   double a, b, c;
-  double root1, root2;
-
   std::cout << "Enter a: ";
   std::cin >> a;
-
   std::cout << "Enter b: ";
   std::cin >> b;
-
   std::cout << "Enter c: ";
   std::cin >> c;
-
-  root1 = (-b + std::sqrt(b*b - 4*a*c)) / (2*a);
-  root2 = (-b - std::sqrt(b*b - 4*a*c)) / (2*a);
-
+  
+  double root1, root2;
+  root1 = std::sqrt(b*b - (4*a*c)); //So you just call the function sqrt() one time.
+  root2 = (-b - root1)/ (2*a);
+  root1 = (-b + root1)/ (2*a);
+  
   std::cout << "Root 1 is " << root1 << "\n";
   std::cout << "Root 2 is " << root2 << "\n";
-
-  return 0;
-  
 }
