35cc3230f236497b5c7d39a5b1d366ff8ac9c233
lethargy-reigns-supreme <82919344+lethargy-reigns-supreme@users.noreply.github.com>
2022-06-27 12:57:44 -0500

    different logic that uses less loops

--- 8-references-and-pointers/bleep/bleep.cpp
+++ 8-references-and-pointers/bleep/bleep.cpp
@@ -3,20 +3,20 @@
 
 #include "functions.hpp"
 
+using namespace std;
+
 int main() {
   
-  std::string word = "broccoli";
-  
-  std::string sentence = "I sometimes eat broccoli. The interesting thing about broccoli is that there are four interesting things about broccoli. Number One. Nobody knows how to spell it. Number Two. No matter how long you boil it, it's always cold by the time it reaches your plate. Number Three. It's green. #broccoli";
-  
-  bleep(word, sentence);
-  
-  for (int i = 0; i < sentence.size(); i++) {
-    
-    std::cout << sentence[i];
-    
-  }
-  
-  std::cout << "\n";
-  
+  // text that will be edited
+  string text = "I sometimes eat broccoli. There are three interesting things about broccoli. Number One. Nobody knows how to spell it. Number Two. No matter how long you boil it, it's always cold by the time it reaches your plate. Number Three. It's green. #broccoli";
+  // word that I will replace
+  string word = "broccoli";
+  // bleep function call
+  bleep(text, word);
+
+  // print edited text
+  std::cout << text << "\n";
+
+  return 0;
+
 }
