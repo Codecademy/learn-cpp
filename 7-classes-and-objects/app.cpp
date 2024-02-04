0adc2dad837d098b3df7bd1bc6665f0748185fd7
Nidu Rasnayake <106425621+Raptor2718@users.noreply.github.com>
2024-02-04 15:01:07 +0000

    Update app.cpp

--- 7-classes-and-objects/app.cpp
+++ 7-classes-and-objects/app.cpp
@@ -1,13 +1,12 @@
-#include <iostream>
 #include "profile.hpp"
+#include <iostream>
+#include <string>
+
 
 int main() {
 
-  Profile sam("Sam Drakkila", 30, "New York", "USA", "he/him");
-  sam.add_hobby("listening to audiobooks and podcasts");
-  sam.add_hobby("playing rec sports like bowling and kickball");
-  sam.add_hobby("writing a speculative fiction novel");
-  sam.add_hobby("reading advice columns");
-  std::cout << sam.view_profile();
+Profile sam("Sam Drakkila", 30, "New York", "he/him");
 
+sam.add_hobbies({"hiking", "reading"});
+std::cout << sam.view_profile() << "\n";
 }
